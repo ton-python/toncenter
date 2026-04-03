c7bff0ba55c18a0e461bd73ee9350426cd665a1e
ness <shonness98@gmail.com>
2026-04-03 13:10:04 +0500

    Update version to 0.4.0

--- toncenter/__meta__.py
+++ toncenter/__meta__.py
@@ -3,6 +3,6 @@
 # This source code is licensed under the MIT License found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "nessshon"
 __url__ = "https://github.com/nessshon/toncenter"
